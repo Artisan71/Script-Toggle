0da130b4079a1f8972ab28f3cb819babb5223220
Artisan71 <155348308+Artisan71@users.noreply.github.com>
2024-04-06 11:34:28 -0800

    Update ScriptEnableAndDisable.cs

--- ScriptEnableAndDisable.cs
+++ ScriptEnableAndDisable.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ScriptEnableAndDisable : MonoBehaviour
 {
+    [Header("MADE BY ARTISAN71 PLS CREDIT IF YOU USE I SPENT -2 BUCKEROONIES ON THIS TO MAKE THIS EAT CHICKEN NUGGET")]
     public string HandTag = "HandTag";
     public MonoBehaviour ObjectToDisableScript;
     
